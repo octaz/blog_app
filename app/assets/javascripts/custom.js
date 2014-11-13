0e2a7d11532e114bb1d695893eddeac2e72127ce
Thomas DeFina <tdef90@gmail.com>
2014-11-12 21:38:10 -0500

    updates left column tags links

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,17 @@
+
+function toggleDisplay(toggleOnId, toggleOffId)
+{
+
+	if (toggleOnId!=null)
+	{
+	
+		document.getElementById(toggleOnId).style.display = "inline";
+	}
+	if (toggleOffId!=null)
+	{
+		
+		document.getElementById(toggleOffId).style.display = "none";
+	}
+}
+
+
